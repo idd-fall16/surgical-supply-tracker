264e1c1c29f8a32b5f900ad02220e429f1150877
Jasper O'Leary <jhaazpr@gmail.com>
2016-11-02 21:52:25 -0700

    basic routing

--- web_app/routes.js
+++ web_app/routes.js
@@ -4,15 +4,38 @@ var path = require('path');
 
 module.exports = function(app) {
     // Server Routes ==================
-    // app.get('/devices', function(req, res) {
-    //     Device.find(function(err, devices) {
-    //         if (err) res.send(err);
-    //         res.json(devices);
-    //         res.status(200).end();
-    //     });
-    // });
+    /**
+     * Uploads a photo to a cart
+     */
+    app.post('/api/:cartId/:photoId', function(req, res) {
+        //asdf
+    });
+
+    /**
+     * Uploads a photo with no cart assignment
+     */
+    app.post('/api/photos/:photoId', function(req, res) {
+      res.send('You sent a photo with id: ' + req.params.photoId).status(200).end();
+    });
 
     // Frontend Routes ===============
+    /**
+     * Displays all photos taken ever
+     */
+    app.get('/api/photos/', function(req, res) {
+      res.send('There will be photos here.').status(200).end();
+    });
+
+    /**
+     * Displays all photos for a particular cart
+     */
+    app.get('/api/:cartId/photos', function(req, res) {
+      //TODO
+    });
+
+    /**
+     * Home page
+     */
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname + '/public/index.html'));
     });
