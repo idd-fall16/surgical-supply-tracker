8578cf1284a8f96b28235fd3670a9bdc96d76dc9
Jasper O'Leary <jhaazpr@gmail.com>
2016-11-05 13:14:12 -0700

    re-add body parsing

--- web_app/routes.js
+++ web_app/routes.js
@@ -1,11 +1,14 @@
 var path = require('path');
+var base64 = require('./base64util')
 //var db = require(__dirname + '/../config/db');
 // var mongoose = require('mongoose');
 
 module.exports = function(app) {
     // Server Routes ==================
     /**
-     * Uploads a photo to a cart
+     * Uploads a photo to a cart.
+     * The photo should be a base64-encoded string in the body of
+     * the request
      */
     app.post('/api/:cartId/:photoId', function(req, res) {
         //asdf
@@ -14,8 +17,10 @@ module.exports = function(app) {
     /**
      * Uploads a photo with no cart assignment
      */
-    app.post('/api/photos/:photoId', function(req, res) {
-      res.send('You sent a photo with id: ' + req.params.photoId).status(200).end();
+    app.post('/api/photos/', function(req, res) {
+      console.log(req.body);
+      //TODO: decoding goes here, save to test maybe
+      res.send('You sent a photo with body string: ' + req.body).status(200).end();
     });
 
     // Frontend Routes ===============
--- web_app/server.js
+++ web_app/server.js
@@ -2,7 +2,7 @@
 var express        = require('express');
 var app            = express();
 //var mongoose       = require('mongoose');
-//var bodyParser     = require('body-parser');
+var bodyParser     = require('body-parser');
 //var methodOverride = require('method-override');
 
 // configuration ===========================================
@@ -14,9 +14,9 @@ var port = process.env.PORT || 3000; // set our port
 //mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
 
 // get all data/stuff of the body (POST) parameters
-// app.use(bodyParser.json()); // parse application/json
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-// app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 
 // app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
