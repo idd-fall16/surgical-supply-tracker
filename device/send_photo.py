0b56629dff97d8ddfb00c90e8cad3fa4fdfc2b20
Jasper O'Leary <jhaazpr@gmail.com>
2016-11-05 16:53:31 -0700

    update send photo to correctly attach file

--- device/send_photo.py
+++ device/send_photo.py
@@ -1,17 +1,10 @@
-
+# Posts a photo to 
 import requests
+
 hostname = 'http://localhost:3000'
 call_string = hostname + '/api/photos/'
+image_path = '/Users/joleary/Desktop/image.jpg'
+files = { 'devicePicture': open(image_path, 'rb') }
 
-with open("/Users/isabelyang/Desktop/img.png", "rb") as f:
-   data = f.read()
-    #print data.encode("base64")
-
-# text = open("test.txt","w") #opens file with name of "test.txt"
-# text.write(data.encode("base64"))
-# text.close()
-
-string_to_send = data.encode("base64")
-
-res = requests.post(call_string, data=string_to_send)
+res = requests.post(call_string, files=files)
 print res
