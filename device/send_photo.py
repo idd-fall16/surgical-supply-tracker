a354c8e3e3d87f85a7a3d91cab02b7b88f735015
Jasper O'Leary <jhaazpr@gmail.com>
2016-11-07 11:04:11 -0800

    generify send_photo for device

--- device/send_photo.py
+++ device/send_photo.py
@@ -1,8 +1,11 @@
-# Posts a photo to 
+# Posts a photo to
 import requests
 
-hostname = 'http://localhost:3000'
-call_string = hostname + '/api/photos/'
+hostname_local = 'http://localhost:3000'
+hostname_cloud = 'http://104.131.159.237:3000'
+call_string = hostname_cloud + '/api/photos/'
+
+# FIXME: add your path to the photo (device specific) here
 image_path = '/Users/joleary/Desktop/image.jpg'
 files = { 'devicePicture': open(image_path, 'rb') }
 
