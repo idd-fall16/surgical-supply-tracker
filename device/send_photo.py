f94b12003cce5e7eb72a4852469b5a7964758428
Jasper O'Leary <jhaazpr@gmail.com>
2016-11-20 16:38:48 -0800

    fix api call in device code

--- device/send_photo.py
+++ device/send_photo.py
@@ -3,10 +3,10 @@ import requests
 
 hostname_local = 'http://localhost:3000'
 hostname_cloud = 'http://104.131.159.237:3000'
-call_string = hostname_cloud + '/api/photos/'
+call_string = hostname_local + '/api/cases/42/photos/'
 
 # FIXME: add your path to the photo (device specific) here
-image_path = '/Users/joleary/Desktop/image.jpg'
+image_path = '/Users/joleary/Desktop/devicePicture.jpg'
 files = { 'devicePicture': open(image_path, 'rb') }
 
 res = requests.post(call_string, files=files)
