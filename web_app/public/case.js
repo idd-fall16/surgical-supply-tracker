90f0065a0479c91958fa7cbffbd9384cce60c213
Jasper O'Leary <jhaazpr@gmail.com>
2016-11-29 18:17:11 -0800

    dynamic routing for cases! also cleanup

--- web_app/public/case.js
+++ web_app/public/case.js
@@ -1,15 +1,7 @@
 (function($){
 
-  var CaseItemList  = Backbone.Model.extend({
-      defaults: {
-
-
-      },
-      url: '/api/cases/1'
-})
-
   var Case = Backbone.Collection.extend({
-    url: '/api/cases/1',
+    url: '/api' + window.location.pathname,
     parse: function(data) {
       console.log("Parsed data.items: " + data.items);
       return data.items;
@@ -26,11 +18,12 @@
       'click #btn_stop_scan': 'stopScanning'
     },
     initialize: function(options){
-      _.bindAll(this, 'render');
       var scope = this;
+      _.bindAll(this, 'render');
+      this.collection = options.collection;
       $(this.el).find(this.el_footer).append("<div class='columns small-3'><button id='btn_stop_scan' href='#' class='button'><h3>STOP SCANNING</h3></button></div>");
-      this.collection = new Case();
       this.listenTo(this.collection, 'add', function() {console.log('change'); this.render()});
+      //FIXME: poll for new data, but try with real time engine
       setInterval(function() {
         scope.collection.fetch();
       }, 1000);
@@ -40,60 +33,16 @@
       var scope = this;
       console.log("render")
       this.case_table.empty();
-      this.case_items = this.model.get("items");
-      //render table view
-
-      //render status and action
-      // $(this.el).find(this.el_footer).append("<div class='columns small-3'><button id='btn_start_scan' href='#' class='button'><h3>BEGIN SCANNING</h3></button></div>");
-
-      // $('#btn_stop_scan').hide();
-      // debugger;
-      console.log("collection: " + JSON.stringify(this.collection));
+      // this.case_items = this.model.get("items");
       this.collection.each(function(model) {
         var case_item = model.attributes;
         var new_case_item = scope.template(case_item);
         scope.case_table.append(new_case_item);
       });
-      // _.each(scope.collection, function(el, idx, list){ // in case collection is not empty
-      //   //self.appendItem(item);
-      //   console.log(el);
-      //   console.log(list);
-      //   var new_case_item = scope.template(case_item);
-      //   scope.case_table.append(new_case_item);
-      // }, this);
     },
     stopScanning: function(){
       console.log("scanning stopped.");
     }
-
-
   });
-
-  var Workspace = Backbone.Router.extend({
-    routes: {
-      "cases/:id":         "getCase",
-      "":                  "home"
-    },
-    getCase : function(id){
-      debugger;
-      console.log("hi");
-      console.log(id);
-    },
-    home: function(){
-      console.log()
-    }
-  })
-  var app_router = new Workspace;
-  var caseView = new CaseView({model: new CaseItemList()});
-
-
-
-
-
-
-
-
-
-
-
+  var caseView = new CaseView({ collection: new Case() });
 })(jQuery);
