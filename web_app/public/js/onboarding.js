dd770176e8ee75c7f9c3939df29c65de0a4731fb
Jessica Chang <jessmchang@gmail.com>
2016-12-06 18:07:16 -0800

    onboarding flow done

--- web_app/public/js/onboarding.js
+++ web_app/public/js/onboarding.js
@@ -1,3 +1,11 @@
+var OnboardingData = Backbone.Collection.extend({
+  url: '/api/case_values',
+  parse: function(data) {
+    return data;
+  }
+});
+
+
 var OnboardingView = Backbone.View.extend({
     // case view consists of two main compoenents: case table, and footer bar.
     el: $('#view_onboarding'),
@@ -8,6 +16,10 @@ var OnboardingView = Backbone.View.extend({
     pg_main: $('#page_content_main'),
     pg_surgery: $('#page_content_surgery'),
     pg_surgeon: $('#page_content_surgeon'),
+    list_surgeries: $('#list_surgeries'),
+    list_surgeons: $('#list_surgeons'),
+    surgery_template: _.template($('#page_content_surgery_template').html()),
+    surgeon_template: _.template($('#page_content_surgeon_template').html()),
     events: {
       'click #btn-new-session': 'newSession',
       'click #btn-old-session': 'oldSession',
@@ -15,17 +27,36 @@ var OnboardingView = Backbone.View.extend({
       'click #btn_to_session': 'toSession'
     },
     initialize: function(options){
-      _.bindAll(this, 'render');
       var scope = this;
       console.log("initialize onboarding view")
+      _.bindAll(this, 'render');
+      this.collection = options.collection;
+      this.listenTo(this.collection, 'add', function() {console.log('change'); this.render()});
+
       $(this.el).find(this.pg_surgery).hide();
       $(this.el).find(this.pg_surgeon).hide();
+      scope.collection.fetch();
     },
     render: function(){
       var scope = this;
 
-      renderSurgery();
-      renderSurgeon();
+      _.each(this.collection.models, function(model) {
+        var surgery_types = model.attributes.surgery_types;
+        var surgeons = model.attributes.surgeons;
+
+        _.each(surgery_types, function(surgery_type){
+            var surgery = scope.surgery_template(surgery_type);
+            scope.list_surgeries.append(surgery);
+        });
+
+        _.each(surgeons, function(surgeon){
+            var surgeon = scope.surgeon_template(surgeon);
+            scope.list_surgeons.append(surgeon);
+        });
+
+
+
+      });
     },
     newSession: function(){
       console.log("new onboarding session");
@@ -54,13 +85,27 @@ var OnboardingView = Backbone.View.extend({
           console.log("done", o);
         });
 
-        window.location.href = '/cases/1' //change to correct id
 
-    },
-    renderSurgery: function(){
-      //render from json here
-    },
-    renderSurgeon: function(){
+        var selected_surgery = $("select#list_surgeries option:selected").text();
+        var selected_surgeon = $("select#list_surgeons option:selected").text();
+        var dataString = {"surgeon" :selected_surgeon , "surgery_type":selected_surgery};
+
+        $.ajax({
+           type: "POST",
+           url: "api/cases",
+           data: JSON.stringify(dataString),
+           contentType: "application/json",
+           success: function(res) {
+            window.location.href = '/cases/' + res.case_number //change to correct id
+           },
+           error: function(e){
+             alert(e);
+             console.log(e);
+           }
+         });
+
+
+        //this.collection.
 
     }
 
